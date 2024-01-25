d973583b041ff599a21547ad505bfca730fbd0a4
melanny0822 <melanny.cordoba@correo.tdea.edu.co>
2024-01-25 18:51:14 -0500

    Configuracion Firebase

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAySGvZQrRjwpaoEmjipnK3OvsWoMKSNHg",
+    authDomain: "web-e-commerce-1c5c6.firebaseapp.com",
+    projectId: "web-e-commerce-1c5c6",
+    storageBucket: "web-e-commerce-1c5c6.appspot.com",
+    messagingSenderId: "625409670975",
+    appId: "1:625409670975:web:2bbc5e2fa97ebaa9361d8a",
+    measurementId: "G-RW7B0MZ9R9"
+}
+
+export default firebaseConfig
--- src/ui/Sidebar.js
+++ src/ui/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className="bg-slate-600">Sidebar</div>
+    <div className='bg-gray-800 p-6 text-white h-screen w-64 text-2xl font-bold'>E-COMMERCE</div>
   )
 }
 
